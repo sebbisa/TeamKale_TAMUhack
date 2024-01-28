6e6e45604e70fc31c4cba6444c945c5c8fab20b3
Lilly Mitchell <lillym6@tamu.edu>
2024-01-27 21:23:00 -0600

    added button

--- crying/hack.py
+++ crying/hack.py
@@ -10,6 +10,15 @@ width, height = 800, 800
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Brain Blast")
 
+color_light = (170, 170,170)
+
+color_dark = (100,100,100)
+
+color = (255, 255, 255)
+smallfont = pygame.font.SysFont('Corbel', 35, bold=False, italic=False)
+
+text = smallfont.render('quit', True, color)
+
 #create game window
 
 bg_image = pygame.image.load('assets/photo_of_bomb.jpeg').convert_alpha()
@@ -24,7 +33,31 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
+        #checks if a mouse is clicked 
+        if event.type == pygame.MOUSEBUTTONDOWN: 
+              
+            #if the mouse is clicked on the 
+            # button the game is terminated 
+            if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40: 
+                pygame.quit() 
+                  
+    # fills the screen with a color 
+    screen.fill((60,25,60)) 
+      
+    # stores the (x,y) coordinates into 
+    # the variable as a tuple 
+    mouse = pygame.mouse.get_pos() 
+      
+    # if mouse is hovered on a button it 
+    # changes to lighter shade  
+    if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40: 
+        pygame.draw.rect(screen,color_light,[width/2,height/2,140,40]) 
+          
+    else: 
+        pygame.draw.rect(screen,color_dark,[width/2,height/2,140,40]) 
+      
+    # superimposing the text onto our button 
+    screen.blit(text , (width/2+50,height/2)) 
     # update display window
     pygame.display.update()
 
